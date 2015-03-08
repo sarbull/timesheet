f2cc5f8c3ae8cf8a71f5d25e03868dbe8eda98dd
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2015-03-08 12:19:57 +0200

    changed title

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Laravel</title>
+		<title>Timesheet</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 		<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
