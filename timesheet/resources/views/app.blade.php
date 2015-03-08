c1ad756dd6b351293064f6b5faace8237b98f517
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2015-03-08 10:17:23 +0200

    added navbar to a partial

--- timesheet/resources/views/app.blade.php
+++ timesheet/resources/views/app.blade.php
@@ -20,61 +20,7 @@
 </head>
 <body>
 
-<div class="container">
-<div class="row">
-<div class="col-md-10 col-md-offset-1">
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="{{ url('/') }}">Timesheet</a>
-			</div>
-
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					@if (Auth::user())
-						<li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Projects <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/projects') }}">List</a></li>
-								<li><a href="{{ url('/projects/create') }}">Create</a></li>
-							</ul>
-						</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Companies <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/companies') }}">List</a></li>
-								<li><a href="{{ url('/companies/create') }}">Create</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						<li><a href="{{ url('/auth/register') }}">Register</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</div>
-	</nav>
-</div>
-</div>
-</div>
+	@include('include.navbar')
 
 	@yield('content')
 
