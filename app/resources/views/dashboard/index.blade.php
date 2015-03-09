9e37dc451989bd93beba00d008b00c090ff352c0
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2015-03-09 23:47:50 +0200

    updated calendar colors

--- app/resources/views/dashboard/index.blade.php
+++ app/resources/views/dashboard/index.blade.php
@@ -26,24 +26,35 @@
       @endif
       <h3>{{ $month }}</h3>
       <table id="calendar" class="table table-bordered">
+          <tr>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+            <th>Sunday</th>
+          </tr>
           @foreach($month as $week)
           <tr>
               @foreach($week as $day)
+                {? $class = ""; ?}
                 @if( $today->format("d") == $day->format("d") )
-                  <td style="background-color:#eee">
-                @else
-                  <td>
+                  {? $class = "background-color:#eee"; ?}
+                @elseif($day->format("M") != $month->format("M"))
+                  {? $class = "color:#ccc"; ?}
                 @endif
-                <div>
-                  {{ $day->format("D")}} <strong>{{ $day->format("d") }}</strong>
-                  @foreach($times as $time)
-                    @if($time->created_at->format('Y-m-d') == $day->format('Y-m-d'))
-                      <br>
-                      <a href="{{route('projects.tickets.show', ['project_id' => $time->ticket->project->id, 'ticket_id' => $time->ticket->id])}}">#{{ $time->ticket->ref_id }}</a>
-                      {{ $time->hour }}h
-                    @endif
-                  @endforeach
-                </div>
+                <td style="{{$class}}">
+                  <div>
+                    <span class="pull-right"><strong>{{ $day->format("d") }}</strong></span>
+                    @foreach($times as $time)
+                      @if($time->created_at->format('Y-m-d') == $day->format('Y-m-d'))
+                        <br>
+                        <a href="{{route('projects.tickets.show', ['project_id' => $time->ticket->project->id, 'ticket_id' => $time->ticket->id])}}">#{{ $time->ticket->ref_id }}</a>
+                        {{ $time->hour }}h
+                      @endif
+                    @endforeach
+                  </div>
                 </td>
               @endforeach
           </tr>
